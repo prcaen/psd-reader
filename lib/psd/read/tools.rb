695392d91f349f97cf66bca1160487fd500fb5da
Pierrick CAEN <prcaen@gmail.com>
2013-04-04 21:32:44 +0200

    Humanize file size & time diff

--- lib/psd/read/tools.rb
+++ lib/psd/read/tools.rb
@@ -4,6 +4,26 @@ module Psd
       def self.padding_2(i)
         (i + 1).floor / 2 * 2;
       end
+
+      def self.format_size(s)
+        units = %W(B KiB MiB GiB TiB)
+
+        size, unit = units.reduce(s.to_f) do |(fsize, _), utype|
+          fsize > 512 ? [fsize / 1024, utype] : (break [fsize, utype])
+        end
+
+        "#{size > 9 || size.modulo(1) < 0.1 ? '%d' : '%.1f'} %s" % [size, unit]
+      end
+
+      def self.format_time_diff(start_time, end_time)
+        diff = end_time - start_time
+
+        if diff.floor == 0
+          "#{(diff * 1000.0).round(2)}ms"
+        else
+          "#{diff.round(2)}s"
+        end
+      end
     end
   end
 end
